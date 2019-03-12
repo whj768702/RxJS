b215af4410705e9dbdf11026bf53124a623fbe51
whj768702 <whj768702@gmail.com>
2019-03-12 18:58:25 +0800

    Observable中的complete  () => void

--- src/observable.ts
+++ src/observable.ts
@@ -1,17 +1,19 @@
 import { Observable } from "rxjs";
 
-const onSubscribe = (observer: Observable) => {
+const onSubscribe = (observer: { next: (arg0: number) => void; complete: () => void; }) => {
     let number = 1;
     const handle = setInterval(() => {
         observer.next(number++);
         if (number > 3) {
             clearInterval(handle);
+            observer.complete();
         }
     }, 1000);
 };
 
 const source$ = new Observable(onSubscribe);
 const theObserver = {
-    next: (item:Number) => console.log(item)
+    next: (item:Number) => console.log(item),
+    complete: () => console.log('no data')
 }
 source$.subscribe(theObserver);
