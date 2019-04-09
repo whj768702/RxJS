888ecb2468d397878d5b797540f9add8aaf74060
whj768702 <whj768702@gmail.com>
2019-04-09 09:13:07 +0800

    windowToggle注释。

--- src/操作符/transformation转化类/windowToggle.ts
+++ src/操作符/transformation转化类/windowToggle.ts
@@ -1,3 +1,9 @@
+/*
+collect and emit observable of values from source between opening and closing emission.
+windowToggle<T, O>(openings: Observable<O>, closingSelector: (openValue: O) => Observable<any>): OperatorFunction<T, Observable<T>>
+每当openings产生一个数据，代表一个缓冲窗口的开始，同时，closingSelector也会被调用，用来
+获得缓冲窗口结束的通知。
+*/
 import { timer, interval } from "rxjs";
 import { windowToggle, tap, mergeAll } from "rxjs/operators";
 
