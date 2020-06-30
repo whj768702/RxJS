3fc28def5c91a57783cb2a8b4f018b5699b3ffdb
whj768702 <whj768702@gmail.com>
2020-06-30 08:59:34 +0800

    refactor(forkJoin): 补充注释说明。

--- src/操作符/combination合并类/forkJoin.ts
+++ src/操作符/combination合并类/forkJoin.ts
@@ -4,16 +4,14 @@ Joins last values emitted by passed Observables.
 Observable对象只产生一个数据，是所有参数Observable
 对象的最后一个数据。
 */
-import { forkJoin, of } from "rxjs";
+/* 如果参数中Observable有不complete的，forkJoin永远不会emit一个值 */
+import { forkJoin, of } from 'rxjs';
 
-const observable = forkJoin(
-  of(1, 2, 3, 4),
-  of(5, 6, 7)
-);
+const observable = forkJoin(of(1, 2, 3, 4), of(5, 6, 7));
 observable.subscribe(
-  value => console.log(value),
-  err => {},
-  () => console.log('complete')
+  (value) => console.log(value),
+  (err) => {},
+  () => console.log('complete'),
 );
 // [4, 7]
 // complete
