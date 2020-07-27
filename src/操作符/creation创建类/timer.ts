b56b94892a2e4a503c2010e48672bd474682b5ae
whj768702 <whj768702@gmail.com>
2020-07-27 15:26:00 +0800

    refactor: 单词修改.

--- src/操作符/creation创建类/timer.ts
+++ src/操作符/creation创建类/timer.ts
@@ -1,6 +1,6 @@
 import { timer } from "rxjs";
 
-const souruce$ = timer(3000, 1000);
-souruce$.subscribe(console.log);
+const source$ = timer(3000, 1000);
+source$.subscribe(console.log);
 //订阅3s后
 //0, 1, 2 ……每秒产生一个数
